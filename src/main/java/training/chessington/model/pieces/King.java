eda22af46e5f9ab3fb5fb00ab3e9230ea4b815ef
CARLISLE Kai <kcarlisle@europe.corp.altran.com>
2018-09-27 16:35:33 +0100

    started looking for king checks, not simulating future moves

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -30,16 +30,54 @@ public class King extends AbstractPiece {
 		for (Coordinates dir : directions) {
 			to = from.plus(dir.getRow(), dir.getCol());
 			if (to.isOnBoard()) {
-				if (!board.isEmpty(to)) {
+				if (board.isEmpty(to)) {
+					moves.add(new Move(from, to));
+				} else {
 					if (!board.get(to).getColour().equals(this.colour)) {
 						moves.add(new Move(from, to));
 					}
-				} else {
-					moves.add(new Move(from, to));
 				}
 			}
 		}
 		
+		moves = filterMoves(moves, board);
+		
 		return moves;
+    }
+    
+    private ArrayList<Move> filterMoves(ArrayList<Move> kingsMoves, Board board) {
+    	Coordinates coords;
+		ArrayList<Move> enemyAvailableMoves = new ArrayList<Move>();
+		ArrayList<Move> illegalMoves = new ArrayList<Move>();
+		
+		outerloop:
+    	for (Move kingsMove : kingsMoves) {
+    		//simulate move
+    		//for each available move of each enemy piece:
+    		//	if their move.to = this move.to, exclude this move form the king's list of available moves
+    		for (int row = 0; row <= 7; row++) {
+    			for (int col = 0; col <= 7; col++) {
+    				coords = new Coordinates(row, col);
+    				if (!board.isEmpty(coords)) {
+    					if (!board.get(coords).getColour().equals(this.colour) && !board.get(coords).getType().equals(PieceType.KING)) {
+    						for (Move enemyMove : board.get(coords).getAllowedMoves(coords, board)) {
+    							if (enemyMove.getTo().equals(kingsMove.getTo())) {
+    								illegalMoves.add(kingsMove);
+    								continue outerloop;
+    							}
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
+		
+		for (Move illegalMove : illegalMoves) {
+			if (kingsMoves.contains(illegalMove)) {
+				kingsMoves.remove(illegalMove);
+			}
+		}
+		
+    	return kingsMoves;
     }
 }
