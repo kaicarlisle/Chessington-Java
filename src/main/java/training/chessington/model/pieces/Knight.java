dc4ff0173750fdcff288a7d37be82f1db332dec8
CARLISLE Kai <kcarlisle@europe.corp.altran.com>
2018-09-27 12:31:24 +0100

    knights movement completed

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -4,32 +4,37 @@ import training.chessington.model.Board;
 import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
-import training.chessington.model.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends AbstractPiece {
-    public Knight(PlayerColour colour) {
-        super(PieceType.KNIGHT, colour);
-    }
+	public Knight(PlayerColour colour) {
+		super(PieceType.KNIGHT, colour);
+	}
 
-    @Override
-    public List<Move> getAllowedMoves(Coordinates from, Board board) {
-    	ArrayList<Move> moves = new ArrayList<Move>();
-    	Vector v;
-    	Coordinates to;
-    	
-    	for (int row = -2; row <= 2; row++) {
-    		for (int col = -2; col <= 2; col++) {
-    			v = new Vector(row, col);
-    			if (v.getMagnitudeSquared() == 5) {
-    				to = new Coordinates(from.getRow() + row, from.getCol() + col);
-    				moves.add(new Move(from, to));
-    			}
-    		}
-    	}
-    	
-        return moves;
-    }
+	@Override
+	public List<Move> getAllowedMoves(Coordinates from, Board board) {
+		ArrayList<Move> moves = new ArrayList<Move>();
+		Coordinates to;
+
+		for (int row = -2; row <= 2; row++) {
+			for (int col = -2; col <= 2; col++) {
+				if ((row*row) + (col*col) == 5) {
+					to = new Coordinates(from.getRow() + row, from.getCol() + col);
+					if (to.getCol()<=7 && to.getCol()>=0 && to.getRow()<=7 && to.getRow()>=0) {
+						if (board.isEmpty(to)) {
+							moves.add(new Move(from, to));
+
+						} else if (!board.get(to).getColour().equals(this.colour)) {
+							moves.add(new Move(from, to));
+						}
+
+					}
+				}
+			}
+		}
+
+		return moves;
+	}
 }
--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -1,7 +1,5 @@
 package training.chessington.model.pieces;
 
-import static training.chessington.model.pieces.PieceAssert.*;
-
 import java.util.List;
 
 import org.junit.Test;
@@ -57,5 +55,122 @@ public class KnightTest {
         assertThat(moves).contains(new Move(coords, coords.plus(1, -2)));
         assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
         assertThat(moves).contains(new Move(coords, coords.plus(-1, -2)));
+    }
+    
+    @Test
+    public void whiteKnightCannotMoveOffBoard() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(0,2);
+        board.placePiece(coords, knight);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -2)));
+    }
+
+    @Test
+    public void blackKnightCannotMoveOffBoard() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(7,2);
+        board.placePiece(coords, knight);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -2)));
+    }
+    
+    @Test
+    public void whiteKnightCanTakeEnemyPieces() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(0,2);
+        board.placePiece(coords, knight);
+        board.placePiece(new Coordinates(2, 3), rook);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -2)));
+    }
+
+    @Test
+    public void blackKnightCanTakeEnemyPieces() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(7,2);
+        board.placePiece(coords, knight);
+        Piece rook = new Rook(PlayerColour.WHITE);
+        board.placePiece(new Coordinates(6,0), rook);
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -2)));
+    }
+    
+    @Test
+    public void whiteKnightCantTakeFriendlyPieces() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(0,2);
+        board.placePiece(coords, knight);
+        board.placePiece(new Coordinates(2, 3), rook);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -2)));
+    }
+
+    @Test
+    public void blackKnightCantTakeFriendlyPieces() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(7,2);
+        board.placePiece(coords, knight);
+        Piece rook = new Rook(PlayerColour.BLACK);
+        board.placePiece(new Coordinates(6,0), rook);
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, -2)));
     }
 }
