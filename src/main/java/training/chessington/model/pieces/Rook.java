ae1a21d8bdbc5395119e9ad8fd6b925b78366d1e
CARLISLE Kai <kcarlisle@europe.corp.altran.com>
2018-09-27 14:30:00 +0100

    bishops and queens movement added (basically identical to rook)

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -16,30 +16,29 @@ public class Rook extends AbstractPiece {
 	@Override
 	public List<Move> getAllowedMoves(Coordinates from, Board board) {
 		ArrayList<Move> moves = new ArrayList<>();
-		Coordinates to=from;
+		Coordinates to = from;
 		ArrayList<Coordinates> directions = new ArrayList<Coordinates>();
-		directions.add(new Coordinates(1,0));
-		directions.add(new Coordinates(-1,0));
-		directions.add(new Coordinates(0,1));
-		directions.add(new Coordinates(0,-1));
+		directions.add(new Coordinates(1, 0));
+		directions.add(new Coordinates(-1, 0));
+		directions.add(new Coordinates(0, 1));
+		directions.add(new Coordinates(0, -1));
+		
 		for (Coordinates dir : directions) {
-
 			to = from.plus(dir.getRow(), dir.getCol());
 			while (to.isOnBoard()) {
 				if (!board.isEmpty(to)) {
-					if ( !board.get(to).getColour().equals(this.colour)) {
-						moves.add(new Move(from,to));
+					if (!board.get(to).getColour().equals(this.colour)) {
+						moves.add(new Move(from, to));
 						break;
 					} else {
 						break;
 					}
 				} else {
-					moves.add(new Move(from,to));
-					to =to.plus(dir.getRow(), dir.getCol());
+					moves.add(new Move(from, to));
+					to = to.plus(dir.getRow(), dir.getCol());
 				}
 			}
 		}
-
 
 		return moves;
 	}
